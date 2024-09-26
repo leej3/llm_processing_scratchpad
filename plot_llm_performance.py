15865548506a8a30c40791dcc43345413885a568
leej3 <johnleenimh@gmail.com>
2024-09-26 16:12:42 +0100

    update plots

--- plot_llm_performance.py
+++ plot_llm_performance.py
@@ -13,40 +13,64 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import plotly.graph_objs as go
 from sklearn.metrics import confusion_matrix
-# df = pd.read_feather("tempdata/llm_extractions/openai-gpt-4o-mini_20240926_120539.feather")
-df = pd.read_feather("tempdata/llm_extractions/openai-gpt-4o-mini-2024-07-18_20240925_144254.feather")[['manual_is_open_data', 'llm_is_open_data']].dropna().astype(bool) #'oddpub_is_open_data'
+# df = pd.read_feather("tempdata/llm_extractions/openai-gpt-4o-mini-2024-07-18_20240925_144254.feather")[['manual_is_open_data', 'llm_is_open_data']].dropna().astype(bool) #'oddpub_is_open_data'
+oddpub = (
+    pd.read_csv("tempdata/oddpub.tsv", sep="\t")
+    .rename(columns={"is_open_data":"oddpub_is_open_data"})
+    .assign(filename= lambda df: df.article.str.split("/").str[-1].str.replace(".txt",".pdf"))
+    [["filename","oddpub_is_open_data"]]
+)
+sonnet35 = (
+    pd.read_feather("tempdata/llm_extractions/anthropic-claude-3.5-sonnet_20240926_140907.intermediate.feather")
+    .rename(columns={"llm_is_open_data":"sonnet35_is_open_data"})
+    [["filename","sonnet35_is_open_data"]]
+)
+df = (
+    pd.read_feather("tempdata/llm_extractions/openai-gpt-4o-mini_20240926_120539.feather")
+    .rename(columns={"llm_is_open_data":"4o-mini_is_open_data"}).merge(
+        sonnet35,on="filename",how="inner"
+    )
+    [['manual_is_open_data', 'sonnet35_is_open_data','4o-mini_is_open_data',"filename"]]
+    .assign(filename = lambda df: df.filename.str.split("/").str[-1])
+    .merge(oddpub,on="filename",how="inner")
+    .dropna()
+    .astype(bool)
+)
 # Assuming 'df' is the dataframe
 # 'oddpub_is_open_data', 'llm_is_open_data', and 'manual_is_open_data' columns
 
 categories = ['True Positive', 'True Negative', 'False Positive', 'False Negative']
 
 
+def calculate_confusion_matrix_values(df, actual_col, predicted_col):
+    cm = confusion_matrix(df[actual_col], df[predicted_col])
+    true_positive = cm[0, 0]
+    true_negative = cm[1, 1]
+    false_positive = cm[1, 0]
+    false_negative = cm[0, 1]
+    values = [true_positive, true_negative, false_positive, false_negative]
+    percentages = [f"{(v / sum(values)) * 100:.1f}%" for v in values]
+    return values, percentages
+
 # Confusion matrix for oddpub
-# cm_oddpub = confusion_matrix(df['manual_is_open_data'], df['oddpub_is_open_data'], labels=[1, 0])
-# # Extracting counts from confusion matrices
-# true_positive_oddpub = cm_oddpub[0, 0]
-# true_negative_oddpub = cm_oddpub[1, 1]
-# false_positive_oddpub = cm_oddpub[1, 0]
-# false_negative_oddpub = cm_oddpub[0, 1]
-# oddpub_values = [true_positive_oddpub, true_negative_oddpub, false_positive_oddpub, false_negative_oddpub]
-# oddpub_percentages = [f"{(v / sum(oddpub_values)) * 100:.1f}%" for v in oddpub_values]
+oddpub_values, oddpub_percentages = calculate_confusion_matrix_values(df, 'manual_is_open_data', 'oddpub_is_open_data')
 
 # Confusion matrix for LLM
-cm_llm = confusion_matrix(df['manual_is_open_data'], df['llm_is_open_data'])
-true_positive_llm = cm_llm[0, 0]
-true_negative_llm = cm_llm[1, 1]
-false_positive_llm = cm_llm[1, 0]
-false_negative_llm = cm_llm[0, 1]
-llm_values = [true_positive_llm, true_negative_llm, false_positive_llm, false_negative_llm]
-llm_percentages = [f"{(v / sum(llm_values)) * 100:.1f}%" for v in llm_values]
+plot_values = {
+"sonnet35": calculate_confusion_matrix_values(df, 'manual_is_open_data', 'sonnet35_is_open_data'),
+"4o-mini": calculate_confusion_matrix_values(df, 'manual_is_open_data', '4o-mini_is_open_data'),
+"oddpub": calculate_confusion_matrix_values(df, 'manual_is_open_data', 'oddpub_is_open_data'),
+
+}
 
 
 # Calculate percentages for text display
 
 # Create the bar chart
 fig = go.Figure(data=[
-    # go.Bar(name='oddpub', x=categories, y=oddpub_values, text=oddpub_percentages, textposition='auto', marker_color='purple'),
-    go.Bar(name='LLM', x=categories, y=llm_values, text=llm_percentages, textposition='auto')
+    go.Bar(name=key, x=categories, y=val[0], text=val[1], textposition='auto') for key, val in plot_values.items()
+    # go.Bar(name='LLM', x=categories, y=llm_values, text=llm_percentages, textposition='auto', marker_color='lightblue')
+
 ])
 
 # Update layout
@@ -58,4 +82,5 @@ fig.update_layout(
 )
 
 # Show the figure
+fig.write_html("llm_performance_comparison.html")
 fig.show()
