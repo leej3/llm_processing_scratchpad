686ff9b4073b3275a161f23ae476306c05b2e44b
leej3 <johnleenimh@gmail.com>
2024-09-26 12:05:34 +0100

    reduce required output

--- run_llms.py
+++ run_llms.py
@@ -85,26 +85,12 @@ class LLMExtractorMetrics(BaseModel):
     year: int = Field(
         description="Best attempt at extracting the year of the publication or use the int 9999",
     )
-    journal: str = Field(description="The journal in which the paper was published")
-    article_type: list[str] = Field(
-        description="The type of article e.g. research article, review, erratum, meta-analysis etc.",
-    )
     country: list[str] = Field(
         description="The countries of the affiliations of the authors",
     )
     institute: list[str] = Field(
         description="The institutes of the affiliations of the authors",
     )
-    doi: str = Field(description="The DOI of the paper")
-    pmid: int = Field(
-        description="The PMID of the paper, use the integer 0 if one cannot be found"
-    )
-    pmcid: int = Field(
-        description="The PMCID of the paper, use the integer 0 if one cannot be found"
-    )
-    title: str = Field(description="The title of the paper")
-    authors: list[str] = Field(description="The authors of the paper")
-    publisher: str = Field(description="The publisher of the paper")
     code_sharing_statement: list[str] = Field(
         description="Statements in the paper that indicate that the code used for analysis has been shared freely online or has not been made available.",
     )
@@ -129,17 +115,6 @@ class LLMExtractorMetrics(BaseModel):
     reasoning_steps: list[str] = Field(
         description="The reasoning steps used to extract the information from the paper. This can verbosely provide context or explanation in the decision making process. Be verbose for this field. Do not leave empty. Each statement should be surrounded by double quotes and use backslash to escape any double quote in the statements themselves. Do not use tabs or newline characters at all.",
     )
-
-    funder: list[str] = Field(
-        description="The funders of the research, may contain multiple funders",
-    )
-    funding_statement: list[str] = Field(
-        description="A statement regarding how the work was funded. This is not always included."
-    )
-    has_funding_statement: bool = Field(
-        description="Whether there is a funding statement in the paper"
-    )
-
 
 
 def get_initial_message(model: str, xml_content: str) -> list[dict]:
